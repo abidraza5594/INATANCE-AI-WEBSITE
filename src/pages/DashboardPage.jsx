5b18fd9218a52d403c7d97264c80269ecd947460
Abid Ansari <abid@gmail.com>
2026-02-14 02:38:46 +0530

    f

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -292,14 +292,6 @@ export default function DashboardPage() {
                                     >
                                         <Check className="w-4 h-4" />
                                         I've Verified
-                                    </button>
-                                </div>
-                                <p className="text-xs text-gray-500 mt-3">
-                                    💡 Check your spam folder if you don't see the email
-                                </p>
-                            </div>
-                        </div>
-                    </div>
                 )}
 
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
